8b29f706c244cf5ab924fb75e8351c6c3410d020
ogel <yocorangelo18@gmail.com>
2023-11-30 21:25:20 +0800

    idk what im doing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+
+import { useState } from 'react';
 import './App.css'
 
 
@@ -216,7 +216,7 @@ const handleFormSubmitEnc = (e) => {
     <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
       <div className="text-center lg:w-2/3 w-full ">
         <h1 className="text-[#d97c30] font-[Montserrat] title-font sm:text-9xl text-6xl mb-4 black">VERI<span className='text-[#CBFFA9]'>SURE</span></h1>
-              <a href="https://github.com/Angelo-misuhara/VeriSure/blob/master/src/App.jsx" target='_blank' rel='noreferrer'>
+              <a href="https://github.com/Angelo-misuhara/VeriSure" target='_blank' rel='noreferrer'>
                 <p className=" text-[#f2f8fd] sm:text-2xl text-[.8rem] font-[Montserrat] leading-relaxed">ENCRYPT,DECRYPT YOUR MESSAGE!</p>
               </a>
               <p className=" text-[#f2f8fd] font-[Poppins] font-light leading-relaxed">from a-z,0-9</p>
@@ -232,7 +232,7 @@ const handleFormSubmitEnc = (e) => {
         <div className="px-6 py-6 md:px-8 md:py-0">
             <h2 className="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100 font-[Montserrat]">ENCRYPTED MESSAGE:</h2>
 
-                  <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">{enc }</p>
+                  <p className="break-all mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">{enc }</p>
         </div>
     </div>
 
@@ -249,33 +249,31 @@ const handleFormSubmitEnc = (e) => {
             
     </div>
         </div>
-        <div>
-    <div className=' container mx-auto flex px-5 pt-[4rem] items-center justify-center flex-col '>
+    <div>
+    <div className=' container mx-auto flex px-5 pt-[5rem] items-center justify-center flex-col'>
       <h1 className="mb-3 text-[#f2f8fd] sm:text-2xl text-[.8rem] font-[Montserrat] leading-relaxed">DECRYPT HERE:</h1>
       <section className="flex flex-col max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 md:flex-row md:h-48">
     <div className="md:flex md:items-center md:justify-center md:w-1/2 md:bg-gray-700 md:dark:bg-gray-800">
         <div className="px-6 py-6 md:px-8 md:py-0">
-            <h2 className="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100 font-[Montserrat]">DECRYPTED MESSAGE:</h2>
+            <h2 className="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100 font-[Montserrat]">ENCRYPTED MESSAGE:</h2>
 
-                  <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">
-                    {dec}
-            </p>
+                  <p className="break-all mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">{dec }</p>
         </div>
     </div>
 
     <div className="flex items-center justify-center pb-6 md:py-0 md:w-1/2 lg:mr-11">
         <form onSubmit={handleFormSubmitDec}>
             <div className="flex flex-col p-1.5 overflow-hidden border rounded-lg dark:border-gray-600 lg:flex-row dark:focus-within:border-blue-300 focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
-                <input className="px-6 py-2 text-[#f2f8fd] placeholder-gray-500 bg-white outline-none dark:bg-gray-800 dark:placeholder-gray-400 focus:placeholder-transparent dark:focus:placeholder-transparent"  onChange={(e) => setDencMessage(e.target.value)}  value={Dencmessage} type="text" name="text" placeholder="Enter your message" aria-label="Enter your message"/>
+                <input className="px-6 py-2 text-[#f2f8fd] placeholder-gray-500 bg-white outline-none dark:bg-gray-800 dark:placeholder-gray-400 focus:placeholder-transparent dark:focus:placeholder-transparent"   onChange={(e) => setDencMessage(e.target.value)}  value={Dencmessage} type="text" name="text" placeholder="Enter your message" aria-label="Enter your email"/>
 
-                <button className="px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">Decrypt</button>
+                <button className="px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">Encrypt</button>
             </div>
                 </form>
     </div>
             </section>
             
     </div>
-  </div>
+        </div>
     
         <div className='flex justify-center  rounded-md py-[5rem]'>
           <div className="relative overflow-x-auto">
